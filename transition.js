8dd9b626209658aaa1d3afa79f15b937a4a2cf99
zawwar mughal <zawwarmehboob56@gmail.com>
2020-05-08 01:14:00 -0700

    some

--- transition.js
+++ transition.js
@@ -8,17 +8,7 @@ $
 
 
     });
-    $(window).scroll(function() {
-        
-        var $dots=$(".name-text");
 
-        if ($(window).scrollTop() > 100){
-$dots.addClass('same-text');
-        }
-        else {($dots.removeClass('same-text',100));
-    }
-       
-    });
 $('.fullpage-nav-list li').mouseover(function(){
 $('.images-container-hidden').addClass('boo');
 });
