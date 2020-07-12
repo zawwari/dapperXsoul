9fcc94c29e466b5c7178ae8ca29022400033763f
zawwarmehboob56@gmail.com <zawwarmehboob56@gmail.com>
2020-07-12 08:31:56 -0700

    scroller addedc

--- js/transition.js
+++ js/transition.js
@@ -1,30 +1,24 @@
-$(document).ready(function() {
-    $('.burger').click (function(){
+$(document).ready(function () {
+    $('.burger').click(function () {
         $(this).toggleClass('active');
         $('.fullpage-nav').toggleClass('show');
         $('.hide').toggleClass('active');
-$
-
-
-
     });
-
-$('.fullpage-nav-list li').mouseover(function(){
-$('.images-container-hidden').addClass('boo');
-});
-$('.fullpage-nav-list li').mouseleave(function(){
-    $('.images-container-hidden').removeClass('boo');
+    $('.fullpage-nav-list li').mouseover(function () {
+        $('.images-container-hidden').addClass('boo');
     });
-
-
-   
+    $('.fullpage-nav-list li').mouseleave(function () {
+        $('.images-container-hidden').removeClass('boo');
+    });
+    $('.searchbar').on('click', function () {
+        $('.searchbar input[type=text]').toggleClass('searchbar-show');
+    });
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 50) {
+            $('.scroll-icon').hide();
+        } 
+        else {
+            $('.scroll-icon').show();
+        }
+    });
 });
-
-$('.searchbar').on('click',function(){
-
-$('.searchbar input[type=text]') .toggleClass('searchbar-show');
-});
-
-
-
-
