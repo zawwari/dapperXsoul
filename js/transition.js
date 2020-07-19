e0e23e45ea76fbfa89c53982601d79ef6a2ef89f
unknown <zawwarmehboob56@gmail.com>
2020-07-19 13:11:22 -0700

    updated site

--- js/transition.js
+++ js/transition.js
@@ -1,24 +1,34 @@
-$(document).ready(function () {
-    $('.burger').click(function () {
+$(document).ready(function() {
+    $('.burger').click(function() {
         $(this).toggleClass('active');
         $('.fullpage-nav').toggleClass('show');
         $('.hide').toggleClass('active');
     });
-    $('.fullpage-nav-list li').mouseover(function () {
+    $('.fullpage-nav-list li').mouseover(function() {
         $('.images-container-hidden').addClass('boo');
     });
-    $('.fullpage-nav-list li').mouseleave(function () {
+    $('.fullpage-nav-list li').mouseleave(function() {
         $('.images-container-hidden').removeClass('boo');
     });
-    $('.searchbar').on('click', function () {
+    $('.searchbar').on('click', function() {
         $('.searchbar input[type=text]').toggleClass('searchbar-show');
     });
-    $(window).scroll(function () {
+    $(window).scroll(function() {
         if ($(window).scrollTop() > 50) {
             $('.scroll-icon').hide();
-        } 
-        else {
+        } else {
             $('.scroll-icon').show();
         }
+    });
+
+    var typed = new Typed('.soul', {
+        strings: ["soul", "you", "men", "looks"],
+        typeSpeed: 100,
+        loop: true,
+        backSpeed: 100,
+        fadeOut: false,
+        fadeOutClass: 'typed-fade-out',
+        fadeOutDelay: 500
+
     });
 });
